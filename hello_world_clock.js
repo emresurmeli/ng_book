b0aa46e5d753c1713e8c72b888d2d00b2ced2c75
Emre Surmeli <s.emre.s.8@gmail.com>
2015-08-19 22:26:53 -0700

    chapter 1

---
+++ hello_world_clock.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html ng-app>
+<head>
+  <title>Simple app</title>
+</head>
+<body>
+  <div ng-controller="MyController">
+    <h1>Hello {{ clock.now }}!</h1>
+  </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.6/angular.js"></script>
+  <script type="text/javascript" src="js/app.js"></script>
+</body>
+</html>
