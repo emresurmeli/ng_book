cb66d453a0a6d8f7e5f8b5eb6cbc721c95d230b4
Emre Surmeli <s.emre.s.8@gmail.com>
2015-08-21 16:32:11 -0700

    add greeting input and set controller

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
-function MyController($scope) {
-  $scope.clock = {
-    now: new Date()
-  };
-  var updateClock = function () {
-    $scope.clock.now = new Date();
-  };
-  setInterval(function () {
-    $scope.$apply(updateClock);
-  }, 1000);
-  updateClock();
-};
+// Create an Angluar module
+
+angular.module('myApp', [])
+.controller('MyController',
+  function ($scope) {
+    $scope.name = '',
+    $scope.clock = {
+      now: new Date()
+    };
+    var updateClock = function () {
+      $scope.clock.now = new Date();
+    };
+    setInterval(function () {
+      $scope.$apply(updateClock);
+    }, 1000);
+    updateClock();
+  }
+);
