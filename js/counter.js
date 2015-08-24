74367c363223d2ecdeadcd0e4af869dff97afef9
Emre Surmeli <s.emre.s.8@gmail.com>
2015-08-23 19:50:42 -0700

    controllers and scope

--- js/counter.js
+++ js/counter.js
@@ -8,4 +8,18 @@ app.controller('FirstController', function($scope) {
   $scope.subtract = function(amount) {
     $scope.counter -= amount;
   };
+  $scope.person = {
+    name: "Emre Surmeli"
+  };
+});
+
+app.controller('ParentController', function($scope) {
+  $scope.person = {greeted: false};
+});
+
+app.controller('ChildController', function($scope) {
+  $scope.sayHello = function() {
+    $scope.person.name = "Emre Surmeli";
+    $scope.person.greeted = true;
+  };
 });
