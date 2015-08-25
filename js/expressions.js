fa1dda487fd92737658063383f5891e3040513a1
emresurmeli <s.emre.s.8@gmail.com>
2015-08-25 15:45:55 -0700

    add interpolation

--- js/expressions.js
+++ js/expressions.js
@@ -1,11 +1,14 @@
-angular.module("myApp", [])
-.controller('MyController', function($scope, $parse) {
-  $scope.$watch('expr', function(newVal, oldVal, scope) {
-    if (newVal !== oldVal) {
-      // Let's set up our parseFun with the expression
-      var parseFun = $parse(newVal);
-      // Get the value of the parsed expression
-      $scope.parsedValue = parseFun(scope);
-    }
-  });
+angular.module('myApp', [])
+  .controller('MyController',
+    function($scope, $interpolate) {
+      $scope.to = 'emre@fullstack.io';
+      $scope.emailBody = 'Hello {{ to }},\n\nMy name is Emre too!';
+      // Set up a watch
+      $scope.$watch('emailBody', function(body) {
+        if (body) {
+          var template = $interpolate(body);
+          $scope.previewText =
+            template({to: $scope.to});
+        }
+      });
 });
