157c56b95da5787d033e07201cc9a103aed3c527
emresurmeli <s.emre.s.8@gmail.com>
2015-08-25 16:06:01 -0700

    add interpolation and start/end symbols

--- js/expressions.js
+++ js/expressions.js
@@ -1,14 +1,33 @@
-angular.module('myApp', [])
-  .controller('MyController',
-    function($scope, $interpolate) {
-      $scope.to = 'emre@fullstack.io';
-      $scope.emailBody = 'Hello {{ to }},\n\nMy name is Emre too!';
-      // Set up a watch
-      $scope.$watch('emailBody', function(body) {
-        if (body) {
-          var template = $interpolate(body);
-          $scope.previewText =
-            template({to: $scope.to});
-        }
-      });
-});
+    angular.module('myApp', ['emailParser'])
+      .controller('MyController',
+        ['$scope', 'EmailParser',
+          function($scope, EmailParser) {
+            $scope.to = 'ari@fullstack.io';
+            $scope.emailBody = 'Hello __to__';
+            // Set up a watch
+            $scope.$watch('emailBody', function(body) {
+              if (body) {
+                $scope.previewText =
+                  EmailParser.parse(body, {
+                    to: $scope.to
+                  });
+              }
+            });
+    }]);
+
+    angular.module('emailParser', [])
+      .config(['$interpolateProvider',
+        function($interpolateProvider) {
+          $interpolateProvider.startSymbol('__');
+          $interpolateProvider.endSymbol('__');
+    }])
+    .factory('EmailParser', ['$interpolate',
+      function($interpolate) {
+        // a service to handle parsing
+        return {
+          parse: function(text, context) {
+            var template = $interpolate(text);
+            return template(context);
+          }
+        };
+    }]);
